e865bdcf82d2de0ed9a8087f5b4fe0e8f7fa62de
h4ck3r <h4ck3r@ghost>
2025-01-10 23:01:47 -0400

    updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,10 +28,10 @@ sudo apt purge -y audacity gimp gnome-games libreoffice* && sudo apt autoremove 
 # Define APT packages
 APT_PACKAGES=(
   asciiart autoconf bat bison btop build-essential cmake cpufetch curl dconf-cli debian-goodies dict
-  dkms fd-find figlet file flatpak font-manager fzf gawk gdebi gh git gir1.2-gtop-2.0 gnome-software-plugin-flatpak
+  dkms fd-find figlet file flatpak font-manager forensics-all fzf gawk gdebi gh git gir1.2-gtop-2.0 gnome-software-plugin-flatpak
   sd rsync gnome-shell-extension-manager gpaste-2 gpg gpgv2 gtk2-engines-murrine httpie imagemagick info linux-headers-$(uname -r) lm-sensors
-  lolcat lynis mitmproxy most nala ncal npm openssl pass patchelf pipx plocate postgresql gir1.2-gtop-2.0 lm-sensors
-  openrc postgresql-contrib procps python-is-python3 rc rkhunter snapd stow tldr terminator tmux ufw thefuck vlc w3m wget wikipedia2text zathura
+  lolcat lynis mitmproxy most nala ncal net-tools npm openssl pass patchelf pipx plocate postgresql gir1.2-gtop-2.0 lm-sensors
+  openrc postgresql-contrib procps python-is-python3 rc rkhunter snapd stow tldr terminator tmux ufw thefuck vlc whois w3m wget wikipedia2text zathura
 )
 
 # Update, upgrade and install APT packages in a single step
