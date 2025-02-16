fe94369a1b95d4d8ca5ebe3a9aeb9ef80e825f30
h4ck3r <h4ck3r@ghostly>
2025-02-16 11:33:53 -0400

    updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,7 +30,7 @@ APT_PACKAGES=(
   apparmor-profiles apparmor-utils asciiart autoconf bat bison btop build-essential cmake cpufetch curl dconf-cli debian-goodies dict clamav
   dkms fd-find figlet file flatpak firejail font-manager forensics-all fzf gawk gdebi gh git gir1.2-gtop-2.0 gnome-software-plugin-flatpak
   sd rsync gnome-shell-extension-manager gpaste-2 gpg gpgv2 gtk2-engines-murrine httpie imagemagick info linux-headers-$(uname -r) lm-sensors
-  lolcat lynis mitmproxy most nala ncal net-tools npm openssl pass patchelf pipx plocate postgresql gir1.2-gtop-2.0 lm-sensors
+  lolcat lynis mitmproxy most nala ncal net-tools npm opensnitch openssl pass patchelf pipx plocate postgresql gir1.2-gtop-2.0 lm-sensors
   yt-dlp hw-probe pandoc pkg-config speedtest-cli postgresql-contrib procps python-is-python3 rc rkhunter snapd stow tldr terminator tmux ufw uptimed thefuck vlc whois w3m wget wikipedia2text zathura
 )
 
