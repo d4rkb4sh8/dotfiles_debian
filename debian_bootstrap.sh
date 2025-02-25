a9a2673b7dfeb72a2efba39049aa9fb102320e63
h4ck3r <h4ck3r@ghostly>
2025-02-25 02:05:25 -0400

    updated

--- debian_bootstrap.sh
+++ debian_bootstrap.sh
@@ -1,68 +1,63 @@
 #!/usr/bin/env bash
 
-set -e # Exit immediately if a command exits with a non-zero status
+# Exit immediately if a command exits with a non-zero status
+set -e
 
+# Function to log messages with a timestamp
 log() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] $*"
 }
-# Install git and others first
-sudo apt install git gh curl gawk cmake -y
 
-# Append 'contrib non-free' to each active deb repository line in sources.list
-log "Adding 'contrib non-free' to active lines in sources.list..."
+# Section: Initial Setup
+log "Starting initial setup..."
+sudo apt install -y git gh curl gawk cmake
 sudo sed -i '/^deb / s/$/ contrib non-free/' /etc/apt/sources.list
 
-# Edit /etc/default/grub for GRUB settings
-log "Configuring GRUB settings..."
-sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/' /etc/default/grub
-sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/a GRUB_CMDLINE_LINUX="rhgb quiet mitigations=off"' /etc/default/grub
-# sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="quiet"/ GRUB_CMDLINE_LINUX_DEFAULT="quiet "' /etc/default/grub
+# Section: Remove Bloatware
+log "Removing bloatware..."
+sudo apt purge -y audacity gimp gnome-games libreoffice*
+sudo apt autoremove -y && sudo apt autoclean -y
 
-# Update GRUB and initramfs
-log "Updating GRUB and initramfs..."
-sudo update-grub
-sudo update-initramfs -u -k all
-
-# Remove bloatware and clean up
-log "Removing bloatware and cleaning up..."
-sudo apt purge -y audacity gimp gnome-games libreoffice* && sudo apt autoremove -y && sudo apt autoclean -y
-
-# Define APT packages
+# Section: Install APT Packages
+log "Installing APT packages..."
 APT_PACKAGES=(
   apparmor-profiles apparmor-utils asciiart autoconf bat bison btop build-essential cmake cpufetch curl dconf-cli debian-goodies dict clamav
   dkms fail2ban fd-find figlet file flatpak firejail font-manager forensics-all fzf gawk gdebi gh git gir1.2-gtop-2.0 gnome-software-plugin-flatpak
   sd rsync gnome-shell-extension-manager gpaste-2 gpg gpgv2 gtk2-engines-murrine httpie imagemagick info linux-headers-$(uname -r) lm-sensors
   lolcat lynis mitmproxy most nala ncal net-tools npm openssl pass patchelf pipx plocate postgresql gir1.2-gtop-2.0 lm-sensors
-  yt-dlp hw-probe pandoc pkg-config speedtest-cli postgresql-contrib procps python-is-python3 rc rkhunter snapd stow tldr terminator tmux ufw uptimed thefuck vlc whois w3m wget wikipedia2text zathura
+  hw-probe pandoc pkg-config speedtest-cli postgresql-contrib procps python-is-python3 rc rkhunter snapd stow tldr terminator tmux ufw uptimed thefuck vlc whois w3m wget wikipedia2text zathura
 )
-
-# Update, upgrade and install APT packages in a single step
-log "Updating, upgrading, and installing packages..."
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt install -y "${APT_PACKAGES[@]}" && sudo apt autoremove -y && sudo apt autoclean -y
 
-# Install latest flathub & flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# Section: Git Projects and Dotfiles Setup
+log "Setting up gitprojects and dotfiles..."
+mkdir -p $HOME/gitprojects
+git clone https://github.com/d4rkb4sh8/main.git $HOME/gitprojects/main
+git clone https://github.com/d4rkb4sh8/notes.git $HOME/gitprojects/notes
+git clone https://github.com/d4rkb4sh8/dotfiles.git $HOME/dotfiles
+cd $HOME/dotfiles && stow --adopt . && git restore .
+cp -r $HOME/gitprojects/main/wallpapers $HOME/Pictures
 
-# Install Snap
-log "Installing Snap..."
-sudo snap install snapd
-sudo snap install snap-store
+# Section: Flatpak and Snap Setup
+log "Setting up Flatpak and Snap..."
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo snap install snapd snap-store
 snap install $(cat $HOME/dotfiles/snap_list.bak)
 
-# Install ble.sh
-cd $HOME
-git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
-make -C ble.sh install PREFIX=~/.local
+# Section: ble.sh Installation
+log "Installing ble.sh..."
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git $HOME/ble.sh
+make -C $HOME/ble.sh install PREFIX=~/.local
 echo 'source ~/.local/share/blesh/ble.sh' >>~/.bashrc
 
-# Install Hack Nerd Font
+# Section: Font Installation
 log "Installing Hack Nerd Font..."
 mkdir -p ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip -O /tmp/Hack.zip
-unzip /tmp/Hack.zip -d ~/.local/share/fonts
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip -O /tmp/Hack.zip
+unzip -q /tmp/Hack.zip -d ~/.local/share/fonts
 fc-cache -fv
 
-# Set up UFW (Uncomplicated Firewall)
+# Section: UFW Setup
 log "Setting up UFW..."
 sudo ufw limit 22/tcp
 sudo ufw allow 80/tcp
@@ -71,115 +66,86 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw enable
 
-# Install Homebrew
+# Section: Homebrew Setup
 log "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Add Homebrew to PATH
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# Install packages using Homebrew
-log "Installing Homebrew packages:"
-brew install gcc
-brew install $(cat $HOME/dotfiles/brew_list.bak)
+# Increase the file descriptor limit to avoid "too many open files" error
+log "Increasing file descriptor limit for Homebrew..."
+ulimit -n 8192 # Increase the limit to 8192 (or higher if needed)
 
-# Install rust
+brew install gcc $(cat $HOME/dotfiles/brew_list.bak)
+
+# Section: Rust Installation
 log "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Install Tela-circle-icons
+# Section: Icon Theme Installation
 log "Installing Tela-circle-icons..."
-cd $HOME/Downloads
-git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
-cd Tela-circle-icon-theme
-./install.sh
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git $HOME/Downloads/Tela-circle-icon-theme
+$HOME/Downloads/Tela-circle-icon-theme/install.sh
 
-# setup gitprojects
-log "setting up gitprojects..."
-mkdir $HOME/gitprojects
-cd $HOME/gitprojects
-git clone https://github.com/d4rkb4sh8/main.git
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/d4rkb4sh8/notes.git
-
-## install dotfiles and stow them.
-cd $HOME
-git clone https://github.com/d4rkb4sh8/dotfiles.git
-cd dotfiles
-stow --adopt .
-git restore .
-
-# add wallpapers
-log "adding wallpapers..."
-cp -r $HOME/gitprojects/main/wallpapers $HOME/Pictures
-
-# setup GRC colors
-log "setting up GRC colors..."
+# Section: GRC Colors Setup
+log "Setting up GRC colors..."
 cd $HOME/gitprojects
 git clone https://github.com/garabik/grc.git
 cd $HOME/gitprojects/grc
 sudo ./install.sh
 sudo cp /etc/profile.d/grc.sh /etc
 
-# setup Starship prompt
-log "setting up Starship..."
+# Section: Starship Prompt Setup
+log "Setting up Starship..."
 curl -sS https://starship.rs/install.sh | sh
 starship preset nerd-font-symbols -o ~/.config/starship.toml
 
-# Install Grub2 custom theme
+# Section: GRUB Custom Theme Installation
 log "Installing Grub theme..."
-cd $HOME/gitprojects
-git clone https://github.com/vinceliuice/grub2-themes.git
-cd $HOME/gitprojects/grub2-themes
+git clone https://github.com/vinceliuice/grub2-themes.git $HOME/gitprojects/grub2-themes
 cp $HOME/Pictures/wallpapers/wallpaper_001.jpg $HOME/gitprojects/grub2-themes/background.jpg
-sudo ./install.sh -s 1080p -b -t whitesur
+sudo $HOME/gitprojects/grub2-themes/install.sh -s 1080p -b -t whitesur
 
-#Install flatpaks
-flatpak install $(cat $HOME/dotfiles/flatpaks_list.bak)
+# Section: GRUB Configuration
+log "Configuring GRUB..."
+sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/' /etc/default/grub
+sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/a GRUB_CMDLINE_LINUX="rhgb quiet mitigations=off"' /etc/default/grub
+sudo update-grub
+sudo update-initramfs -u -k all
 
-# Install Orchis theme
-#cd $HOME/gitprojects/
-#git clone https://github.com/vinceliuice/Orchis-theme.git
-#cd Orchis-theme
-#./install.sh -t red -c dark -s compact -l --tweaks compact --shell
+# Section: Flatpak Applications Installation
+log "Installing Flatpak applications..."
+flatpak install $(cat $HOME/dotfiles/flatpaks_list.bak) -y
 
-# Install GTFOB lookup
-#log "Installing GTFOB..."
-pipx install git+https://github.com/nccgroup/GTFOBLookup.git
-
-# cargo install
-cargo install cargo-update
-cargo install cargo-list
-cargo install kanata --version 1.6.0
-
-# Binsider
-cargo install binsider
-
-#Install tgpt
-curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
-
-# Install atuin
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-
-# Install Ollama
-curl -fsSL https://ollama.com/install.sh | sh
-
-# Final update and clean up
-log "Final update and clean up..."
-sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
-
-# Source .bashrc
-log "Sourcing .bashrc..."
 source $HOME/.bashrc
 
-# Gnome restore
+# Section: Cargo Installations
+log "Installing cargo packages..."
+cargo install cargo-update cargo-list kanata binsider
+
+# Section: tgpt and Atuin Installation
+log "Installing tgpt and atuin..."
+curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
+# Section: Ollama Installation
+log "Installing Ollama..."
+curl -fsSL https://ollama.com/install.sh | sh
+
+# Section: Final Update and Cleanup
+log "Final update and cleanup..."
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+# Section: Source .bashrc and Restore Gnome Settings
+log "Sourcing .bashrc and restoring Gnome settings..."
+source $HOME/.bashrc
 dconf load / <$HOME/dotfiles/gnome_settings.bak
 
-# Apparmor
+# Section: AppArmor Enforcement
+log "Enforcing AppArmor profiles..."
 sudo aa-enforce /etc/apparmor.d/*
 
-# Display message
+# Section: Display Completion Message and Reboot
+log "Displaying completion message and rebooting..."
 figlet h4ck3r m4ch1n3 | lolcat
-
 sudo reboot
