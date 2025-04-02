555ca87a07312829e4496f9865b10fa4f0745d71
d4rkb4sh8 <d4rkb4sh8@gmail.com>
2025-04-02 08:25:33 -0400

    updated

--- backups/settings_backup.sh
+++ backups/settings_backup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 #gnome settings
-dconf dump / >$HOME/dotfiles/gnome_settings.bak
+dconf dump / >$HOME/dotfiles/backups/gnome_settings.bak
 
 #apt_list
-apt list --installed | cut -d "/" -f1 | grep -v "Listing..." >$HOME/dotfiles/apt_list.bak
+apt list --installed | cut -d "/" -f1 | grep -v "Listing..." >$HOME/dotfiles/backups/apt_list.bak
 
 #flatpaks_list
-flatpak list --app --columns=application >$HOME/dotfiles/flatpaks_list.bak
+flatpak list --app --columns=application >$HOME/dotfiles/backups/flatpaks_list.bak
 
 # brew list
-brew list -1 >$HOME/dotfiles/brew_list.bak
+brew list -1 >$HOME/dotfiles/backups/brew_list.bak
 
 #snap list
-snap list | awk 'NR>1{print $1}' >$HOME/dotfiles/snap_list.bak
+snap list | awk 'NR>1{print $1}' >$HOME/dotfiles/backups/snap_list.bak
